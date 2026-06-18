4850eb9bfbd45c9c0a42707d0155d9c18433b1da
r0chaya <119661388+r0chaya@users.noreply.github.com>
2026-06-18 23:27:49 +0700

    Add Solution: 1789. Primary Department for Each Employee

---
+++ 1789. Primary Department for Each Employee/18-06-2026_1789.sql
@@ -0,0 +1,13 @@
+// Problem: 1789. Primary Department for Each Employee (Easy)
+// Link: https://leetcode.com/problems/primary-department-for-each-employee
+
+SELECT employee_id,
+       department_id
+FROM Employee
+WHERE primary_flag = 'Y'
+   OR employee_id IN (
+       SELECT employee_id
+       FROM Employee
+       GROUP BY employee_id
+       HAVING COUNT(*) = 1
+   );
